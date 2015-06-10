b689c81e343b275866db4696c2df96f19760920e
Robert Phipps <robert@phippsnet.co>
2015-06-10 19:52:56 +0100

    Removed ID generation
    Changed database to auto increment the ID for us, so we don't have to
    generate that manually.

--- writeposthandler.php
+++ writeposthandler.php
@@ -17,27 +17,17 @@ echo "Connected successfully";
 echo "<hr/>";
 echo "Writing responses to table<br/>";
 
-$lastIDobject = mysqli_query($conn,"SELECT valueint FROM sys WHERE setting='lastPost'");
-
-$lastIDarray = mysqli_fetch_array($lastIDobject);
-
-$nextID = $lastIDarray['valueint'] + 1;
-
-$ID = $nextID;
 $Title = addslashes($_POST["Title"]);
 $Category = addslashes($_POST["Category"]);
 $User = addslashes($_POST["User"]);
 $Content = addslashes($_POST["Content"]);
 $Image = addslashes($_POST["Image"]);
 
-$sql = "INSERT INTO posts (ID, Title, Category, User, Content, Image)
-VALUES ('".$ID."', '".$Title."', '".$Category."', '".$User."', '".$Content."', '".$Image."')";
+$sql = "INSERT INTO posts (Title, Category, User, Content, Image)
+VALUES ('".$Title."', '".$Category."', '".$User."', '".$Content."', '".$Image."')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
-    mysqli_query($conn, "UPDATE sys
-            SET valueint='".$nextID."'
-            WHERE setting='lastPost'");
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
