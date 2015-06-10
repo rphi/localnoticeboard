63a5f997ce0bd229ea51c586da016973cdb42f53
Robert Phipps <robert@phippsnet.co>
2015-06-10 19:51:43 +0100

    New SQL initialisation script
    This *should* create a couple of tables in your database to allow the
    other stuff to work.

---
+++ initsql.php
@@ -0,0 +1,55 @@
+<?php
+
+include ("servercreds.php");
+
+// Run this to initialise the MySQL database
+
+echo "Connecting to SQL server @ ".$servername." into database ".$dbname."<br/>";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+// sql to create table
+$sql = "CREATE TABLE posts (
+ID INT(5) PRIMARY KEY AUTO_INCREMENT,
+Title text,
+Category text,
+Timestamp TIMESTAMP default CURRENT_TIMESTAMP,
+User text,
+Content text,
+Image text,
+Rating INT(5) default '0'
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table 'posts' created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+echo "<br/> Creating 'sys' table";
+
+// sql to create table
+$sql = "CREATE TABLE sys (
+setting text NOT NULL,
+valuetxt text default NULL,
+valueint int(11) default NULL,
+valuebool boolean default NULL
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table 'sys' created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+
+?>
